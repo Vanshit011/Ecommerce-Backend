06e3e86ec561ea0484c60d38a44c9f2dc0dcb68d
Vanshit Patel <vanshitpatel10@gmail.com>
2026-02-24 18:22:22 +0530

    feat: implement Google OAuth authentication strategy

--- src/module/auth/strategies/google.strategy.ts
+++ src/module/auth/strategies/google.strategy.ts
@@ -18,12 +18,12 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     });
   }
 
-  async validate(
+  validate(
     accessToken: string,
     refreshToken: string,
     profile: any,
     done: VerifyCallback,
-  ): Promise<any> {
+  ): any {
     const { name, emails, id } = profile;
     const user = {
       email: emails[0].value,
